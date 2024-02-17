4b18ccaadabb10fb6bae9444bb58c344e897102b
ben <x_z_gfree@163.com>
2024-02-17 11:48:37 +0800

    feature: build package config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,28 @@
 // vite.config.ts
-import UnoCSS from "unocss/vite";
+import { resolve } from "path";
 import { defineConfig } from "vite";
-
+import dts from "vite-plugin-dts";
 export default defineConfig({
-  plugins: [UnoCSS()],
+  build: {
+    lib: {
+      entry: [
+        resolve(__dirname, "src/core/index.ts"),
+        resolve(__dirname, "src/core/dom.ts"),
+        resolve(__dirname, "src/core/signal.ts"),
+      ],
+      formats: ["es"],
+      fileName(format, entryName) {
+        return `${entryName}.${format}.js`;
+      },
+    },
+  },
+  plugins: [
+    // UnoCSS(),
+    dts({
+      insertTypesEntry: true,
+      rollupTypes: true,
+    }),
+  ],
   define: {
     __DEV__: true,
   },
